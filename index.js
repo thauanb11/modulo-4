f50aa1e6aff443c53e15c7cdf508fd685465b3f3
thauanb11 <guedesbruno40@gmail.com>
2024-01-08 12:31:46 -0300

    aprendendo sobre o debug, novos metodos de where

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const app = express();
 
 
 app.get('/', async (req, res) => {
-    const agenda = await knex('agenda');
+    // conexao.query ('select * from agenda')
+    // .debug sempre usado no knex.
+    // const agenda = await knex('agenda').debug();
+    // const agenda = await knex('agenda').where('id', 5).debug();
+    // const agenda = await knex('agenda').where('id', '!=', 5).debug();
+    // const agenda = await knex('agenda').where({ id: 5 }).first().debug();
+
+    const agenda = await knex('agenda').where({ id: 5 }).select('id', 'nome').first().debug();
+
+
+
     return res.json(agenda);
 });
 
